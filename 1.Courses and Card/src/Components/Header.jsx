2becf81d4a58538425ff83dd3045dcc985f5acf0
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-06-05 15:40:50 +0400

    Create Header.jsx

---
+++ 1.Courses and Card/src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div
+      style={{
+        width: "100wh",
+        height: "100px",
+        backgroundColor: "blue",
+        display: "flex",
+        justifyContent: "start",
+        alignItems: "center",
+        paddingLeft: "20px",
+      }}
+    >
+      <h1>Header</h1>
+    </div>
+  );
+}
+
+export default Header;
