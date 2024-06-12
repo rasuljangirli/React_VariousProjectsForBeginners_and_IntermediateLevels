b2dd4d80b1b59f94ec46427686ddd669f7e20a33
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-06-12 23:45:06 +0400

    Create Language.jsx

---
+++ 7.Programming Languages/src/components/Language.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { GlobalContex } from "../contax/GlobalContax";
+
+function Language({ language }) {
+  const { id, languageName, description } = language;
+  const { deleteItem } = useContext(GlobalContex);
+
+  const deleteItemID = () => {
+    deleteItem(id);
+  };
+
+  return (
+    <div className="languageItem">
+      <div>{languageName}</div>
+      <div>{description}</div>
+      <button className="btn" onClick={deleteItemID}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default Language;
