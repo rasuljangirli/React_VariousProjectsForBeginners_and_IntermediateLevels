df1ff27df6b25e028eb3d8469b79567950a39c23
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-06-17 22:18:56 +0400

    fff

---
+++ 10.Custom hooks form/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import useInput from "./hooks/useInput";
+
+function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  const [firstName, bindFirstName, resetFirstName] = useInput("");
+  const [lastName, bindLastName, resetLastName] = useInput("");
+
+  const showAlert = (e) => {
+    e.preventDefault();
+    alert(`Hello : ${firstName} ${lastName}`);
+    resetFirstName();
+    resetLastName();
+  };
+
+  return (
+    <div>
+      <form onSubmit={showAlert} action="">
+        <input {...bindFirstName} />
+        <input {...bindLastName} />
+        <button>click me</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
